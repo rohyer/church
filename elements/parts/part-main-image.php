d4ef550b68e038f7b84e6a2757722de62ff7fad6
rohyer <guilhermerl.dev@gmail.com>
2019-10-04 21:55:36 -0300

    Página Home - Ajustes do vídeo

--- elements/parts/part-main-image.php
+++ elements/parts/part-main-image.php
@@ -1,6 +1,6 @@
 <section id="main-image">
 
-	<video loop autoplay muted id="video" class="bg-video">
+	<video loop autoplay muted poster="<?php echo get_template_directory_uri(); ?>/assets/video/main-img-video.png" id="video" class="bg-video">
 		<source src="<?php echo get_template_directory_uri(); ?>/assets/video/luterana-video.webm" type="video/webm">
 		<source src="<?php echo get_template_directory_uri(); ?>/assets/video/luterana-video.mp4" type="video/mp4">
 	</video>
