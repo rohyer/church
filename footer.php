f4d3feffe51deac6cdf1752d842e15693f211948
rohyer <guilhermerl.dev@gmail.com>
2020-01-03 13:54:46 -0300

    Ajustes responsivos

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 		<div class="container-custom">
 			<div class="row">
 				<div id="footer-content">
-					<ul class="col-4">
+					<ul class="col-xl-4 col-lg-4 col-md-3 col-sm-6">
 						<h6>Menu</h6>
 						<li><a href="#">Home</a></li>
 						<li><a href="#">Programas & Eventos</a></li>
@@ -10,14 +10,14 @@
 						<li><a href="#">Contato</a></li>
 					</ul>
 
-					<ul class="col-4">
+					<ul class="col-xl-4 col-lg-4 col-md-4 col-sm-6">
 						<h6>Contato</h6>
 						<li>Email: <?php echo get_theme_mod( 'email_section' ); ?></li>
 						<li>Telefone: <?php echo get_theme_mod( 'telefone_section' ); ?></li>
 						<li>Whatsapp: <?php echo get_theme_mod( 'whatsapp_section' ); ?></li>
 					</ul>
 
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3720.0419838767966!2d-50.45447538503607!3d-21.190491084208734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94964472bb935ec3%3A0x551a62ecad171125!2sRua+Ant%C3%B4nio+Corbucci+-+Novo+Para%C3%ADso%2C+Ara%C3%A7atuba+-+SP%2C+16074-260!5e0!3m2!1spt-BR!2sbr!4v1562014773227!5m2!1spt-BR!2sbr" class="col-4" width="400" height="240" frameborder="0" style="border:0" allowfullscreen></iframe>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3720.0419838767966!2d-50.45447538503607!3d-21.190491084208734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94964472bb935ec3%3A0x551a62ecad171125!2sRua+Ant%C3%B4nio+Corbucci+-+Novo+Para%C3%ADso%2C+Ara%C3%A7atuba+-+SP%2C+16074-260!5e0!3m2!1spt-BR!2sbr!4v1562014773227!5m2!1spt-BR!2sbr" class="col-xl-4 col-lg-4 col-md-5" width="400" height="240" frameborder="0" style="border:0" allowfullscreen></iframe>
 				</div>
 			</div>
 		</div>
